cc325b4808d16c612970501613b8ff6a6c2214a1
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2026-03-07 01:01:50 +0000

    fox: Fix CI for release

--- src/arxlang_jupyter_kernel/__init__.py
+++ src/arxlang_jupyter_kernel/__init__.py
@@ -1,6 +1,8 @@
-"""Top-level package for arxlang-jupyter-kernel."""
+"""
+title: Top-level package for arxlang-jupyter-kernel.
+"""
 
 from .kernel import ArxKernel
 
 __all__ = ["ArxKernel", "__version__"]
-__version__ = "0.1.0"
+__version__ = "0.1.0"  # semantic-release
